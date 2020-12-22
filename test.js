803b925330755ee72bf78da0bbd3700622ff308c
balaniyash <58064664+balaniyash@users.noreply.github.com>
2020-12-22 12:37:11 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,9 @@
 console.log("this works")
+			function receiveMessage(evt) {
+				if (evt.origin === 'https://domainone-firebase.web.app') {
+					_tiluuid = evt.data;
+					localStorage.setItem('_tiluuid', _tiluuid);
+				}
+			}
+			window.addEventListener('message', receiveMessage, false);
+			document.getElementById("_tiluuid_frame").src='https://domainone-firebase.web.app';
