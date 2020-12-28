98794d3528252825072707cf61e7970320d696c0
balaniyash <58064664+balaniyash@users.noreply.github.com>
2020-12-28 14:49:37 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,11 +1,15 @@
 console.log('Test Script Started')
+console.log(document.location.href)
 function receiveMessage(evt) {
 	if (evt.origin === 'https://tilanalytics.timesinternet.in') {
 		_tiluuid = evt.data;
 		console.log('my custom user id: '+_tiluuid)
 		localStorage.setItem('_tiluuid', _tiluuid);
+    localStorage.setItem('_til_uuid_timestamp', new Date().getTime())
 	}
 }
 window.addEventListener('message', receiveMessage, false);
 document.getElementById("_tiluuid_frame").src='https://tilanalytics.timesinternet.in/frame_v3.min.html';
 console.log('Test Script Ended')
+
+
